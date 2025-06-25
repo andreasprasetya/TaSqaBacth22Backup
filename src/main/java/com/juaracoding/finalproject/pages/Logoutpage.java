65cf212487f454ca16c556355d0385c5cb53d641
Andreas Prasetya <andreasprasetya20@gmail.com>
2025-06-25 17:04:25 +0700

    first commit

---
+++ src/main/java/com/juaracoding/finalproject/pages/Logoutpage.java
@@ -0,0 +1,51 @@
+package com.juaracoding.finalproject.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Logoutpage {
+    WebDriver driver;
+
+    @FindBy(id = "user-name")
+    WebElement usernameField;
+
+    @FindBy(id = "password")
+    WebElement passwordField;
+
+    @FindBy(id = "login-button")
+    WebElement loginButton;
+
+    @FindBy(xpath = "//h3[@data-test='error']")
+    WebElement errorMessage;
+
+    public Logoutpage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void setUsername(String value) {
+        usernameField.sendKeys(value);
+    }
+
+    public void setPassword(String password) {
+        passwordField.sendKeys(password);
+    }
+
+    public void onClick() {
+        loginButton.click();
+    }
+
+    public void login(String username, String password) {
+        setUsername(username);
+        setPassword(password);
+        onClick();
+    }
+
+    public String getErrorMessage() {
+        return errorMessage.getText();
+    }
+}
+
+
