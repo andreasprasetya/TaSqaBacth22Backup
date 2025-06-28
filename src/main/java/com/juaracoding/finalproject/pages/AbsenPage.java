2d8f697075b7be0b1efb2199496ebe63458edaa1
Andreas Prasetya <andreasprasetya20@gmail.com>
2025-06-28 19:45:08 +0700

    first commit

--- src/main/java/com/juaracoding/finalproject/pages/AbsenPage.java
+++ src/main/java/com/juaracoding/finalproject/pages/AbsenPage.java
@@ -1,83 +1,83 @@
-//package com.juaracoding.finalproject.pages;
-//
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.support.FindBy;
-//import org.openqa.selenium.support.PageFactory;
-//import org.openqa.selenium.support.ui.Select;
-//
-//import java.time.LocalTime;
-//import java.time.format.DateTimeFormatter;
-//
-//
-//public class AbsenPage {
-//  WebDriver driver;
-//
-//    @FindBy(xpath = "/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/button[1]")
-//    WebElement buttonAbsen;
-//
-//  @FindBy(xpath = "/html/body/div[2]/div[3]/div/div/div/button")
-//    WebElement  buttonCamera;
-//
-//  @FindBy(xpath = "/html/body/div[2]/div[3]/div[2]/form/div[1]")
-//    WebElement  timePicker;
-//
-//  @FindBy(xpath = "/html/body/div[2]/div[3]/div[2]/form/div[2]")
-//    WebElement dropDown;
-//
-//  @FindBy(xpath = "//textarea[@placeholder='Note'] ")//input[@name='note'] (tergantung tag-nya)
-//     WebElement noteTextField;
-//
-//    @FindBy(xpath = "/html/body/div[2]/div[3]/div[2]/form/button")
-//    WebElement buttonAbsenMasuk;
-//
-//
-//  public AbsenPage(WebDriver driver) {
-//    this.driver = driver;
-//    PageFactory.initElements(driver, this);
-//  }
-//
-//    public void  onClickAbsen() {
-//        buttonAbsen.click();
-//    }
-//
-//    public void  onClickCamera() {
-//        buttonCamera.click();
-//    }
-//
-//  public int isiJamMasukOtomatis() {
-//      LocalTime now = LocalTime.now();
-//      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
-//      String jam = now.format(formatter);
-//      timePicker.clear();
-//      timePicker.sendKeys(jam);
-//      System.out.println("Jam masuk otomatis diisi: " + jam);
-//      return 0;
-//  }
-//
-//    public void pilihDariDropdown(String value) {
-//        Select select = new Select(dropDown);
-//        select.selectByVisibleText(value);
-//    }
-//    public void isiNote(String note) {
-//        noteTextField.clear();
-//        noteTextField.sendKeys(note);
-//    }
-//          public void onclickAbsenMasuk() {
-//              buttonAbsenMasuk.click();
-//          }
-//
-//    public String getJamMasuk() {
-//        return timePicker.getAttribute("value");
-//    }
-//
-//    public String getNote() {
-//        return noteTextField.getAttribute("value");
-//    }
-//
-//    public String getSelectedDropdownValue() {
-//        Select select = new Select(dropDown);
-//        return select.getFirstSelectedOption().getText();
-//    }
-//
-//}
+package com.juaracoding.finalproject.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+
+public class AbsenPage {
+  WebDriver driver;
+
+    @FindBy(xpath = "/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/button[1]")
+    WebElement buttonAbsen;
+
+  @FindBy(xpath = "/html/body/div[2]/div[3]/div/div/div/button")
+    WebElement  buttonCamera;
+
+  @FindBy(xpath = "/html/body/div[2]/div[3]/div[2]/form/div[1]")
+    WebElement  timePicker;
+
+  @FindBy(xpath = "/html/body/div[2]/div[3]/div[2]/form/div[2]")
+    WebElement dropDown;
+
+  @FindBy(xpath = "//textarea[@placeholder='Note'] ")//input[@name='note'] (tergantung tag-nya)
+     WebElement noteTextField;
+
+    @FindBy(xpath = "/html/body/div[2]/div[3]/div[2]/form/button")
+    WebElement buttonAbsenMasuk;
+
+
+  public AbsenPage(WebDriver driver) {
+    this.driver = driver;
+    PageFactory.initElements(driver, this);
+  }
+
+    public void  onClickAbsen() {
+        buttonAbsen.click();
+    }
+
+    public void  onClickCamera() {
+        buttonCamera.click();
+    }
+
+  public int isiJamMasukOtomatis() {
+      LocalTime now = LocalTime.now();
+      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+      String jam = now.format(formatter);
+      timePicker.clear();
+      timePicker.sendKeys(jam);
+      System.out.println("Jam masuk otomatis diisi: " + jam);
+      return 0;
+  }
+
+    public void pilihDariDropdown(String value) {
+        Select select = new Select(dropDown);
+        select.selectByVisibleText(value);
+    }
+    public void isiNote(String note) {
+        noteTextField.clear();
+        noteTextField.sendKeys(note);
+    }
+          public void onclickAbsenMasuk() {
+              buttonAbsenMasuk.click();
+          }
+
+    public String getJamMasuk() {
+        return timePicker.getAttribute("value");
+    }
+
+    public String getNote() {
+        return noteTextField.getAttribute("value");
+    }
+
+    public String getSelectedDropdownValue() {
+        Select select = new Select(dropDown);
+        return select.getFirstSelectedOption().getText();
+    }
+
+}
--- src/test/java/com/juaracoding/finalproject/steps/AuthenticationLogoutTestStep.java
+++ src/test/java/com/juaracoding/finalproject/steps/AuthenticationLogoutTestStep.java
@@ -1,47 +1,47 @@
-package com.juaracoding.finalproject.steps;
-
-import com.juaracoding.finalproject.DriverSingleton;
-import com.juaracoding.finalproject.pages.LogoutPage;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
-
-import java.time.Duration;
-
-public class AuthenticationLogoutTestStep {
-
-    WebDriver driver;
-    LogoutPage LogoutPage;
-    WebDriverWait wait;
-
-    @Given("User sudah masuk ke dalam halam home")
-    public void testStep01() {
-        driver = DriverSingleton.createOrGetDriver();
-        driver.get("https://magang.dikahadir.com/apps/absent");
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        LogoutPage = new LogoutPage(driver);
-
-    }
-
-    @When("Klik menu bar")
-    public void testStep02() throws InterruptedException {
-        LogoutPage.onMenuClick(); // klik tombol ☰
-        Thread.sleep(1000);
-    }
-
-    @When("Tunggu sampai muncul side bar")
-    public void testStep03() {
-        wait.until(ExpectedConditions.visibilityOf(LogoutPage.onLogoutClick()));
-    }
-
-    @Then("Klik Button logout")
-    public void testStep04() {
-        String expected = "https://magang.dikahadir.com/absen/login";
-        String actual = driver.getCurrentUrl();
-        Assert.assertEquals(actual, expected);
-    }
-}
+//package com.juaracoding.finalproject.steps;
+//
+//import com.juaracoding.finalproject.DriverSingleton;
+//import com.juaracoding.finalproject.pages.LogoutPage;
+//import io.cucumber.java.en.Given;
+//import io.cucumber.java.en.Then;
+//import io.cucumber.java.en.When;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.support.ui.ExpectedConditions;
+//import org.openqa.selenium.support.ui.WebDriverWait;
+//import org.testng.Assert;
+//
+//import java.time.Duration;
+//
+//public class AuthenticationLogoutTestStep {
+//
+//    WebDriver driver;
+//    LogoutPage LogoutPage;
+//    WebDriverWait wait;
+//
+//    @Given("User sudah masuk ke dalam halam home")
+//    public void testStep01() {
+//        driver = DriverSingleton.createOrGetDriver();
+//        driver.get("https://magang.dikahadir.com/apps/absent");
+//        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+//        LogoutPage = new LogoutPage(driver);
+//
+//    }
+//
+//    @When("Klik menu bar")
+//    public void testStep02() throws InterruptedException {
+//        LogoutPage.onMenuClick(); // klik tombol ☰
+//        Thread.sleep(1000);
+//    }
+//
+//    @When("Tunggu sampai muncul side bar")
+//    public void testStep03() {
+//        wait.until(ExpectedConditions.visibilityOf(LogoutPage.onLogoutClick()));
+//    }
+//
+//    @Then("Klik Button logout")
+//    public void testStep04() {
+//        String expected = "https://magang.dikahadir.com/absen/login";
+//        String actual = driver.getCurrentUrl();
+//        Assert.assertEquals(actual, expected);
+//    }
+//}
